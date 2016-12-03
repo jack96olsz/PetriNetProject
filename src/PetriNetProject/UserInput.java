0f5313518e8a9ba0687bc3dfd1a3a9a5e09eeef9
jack96olsz <jack96olsz@gmail.com>
2016-12-03 14:15:08 -0500

    Finished error Checking
    -Reorganized code
    -Checks for negative integers
    -Checks for non-integer input
    -Checks that input is valid
    	-At least 2 places
    	-At least 1 transition

--- src/PetriNetProject/UserInput.java
+++ src/PetriNetProject/UserInput.java
@@ -7,16 +7,21 @@ public class UserInput {
 	private Scanner scan; 			// For scanning user input
 	private int places;				// # of places entered by user
 	private int transitions;		// # of transitions entered by user
-	private String input;			// (Not used yet)
-	private String output;			// (Not used yet)
+	private String input;			// Input for all transitions
+	private String output;			// Output for all transitions
 	private int[] initialMarking;	// Marking entered by user
 	
+	
+	//Constructor
 	public UserInput(){
 		scan = new Scanner(System.in);
 		places = 1;
 		transitions = 0;
+		input = "";
+		output = "";
 	}
 	
+	// Get User input
 	public String getInput(){
 		String outputString = "";	// String for summary of input
 		
@@ -24,70 +29,90 @@ public class UserInput {
 		System.out.println("Input Petri Net information");
 		
 		// Ask for user input until a valid number is entered
+		// loop for number of times equal to the value of places
 		while (places <= 1){
 			System.out.println("Enter a number of places greater than 1:");
-			if(scan.hasNextInt()){
-				places = scan.nextInt();
-			}
-			else {
-				scan.next();
-			}
+			places = askForInt();
 		}
-		
+		// loop for number of times equal to the value of transitions
 		while (transitions <= 0){
 			System.out.println("Enter a number of transitions greater than 0:");
-			if(scan.hasNextInt()){
-				transitions = scan.nextInt();
-			}
-			else {
-				scan.next();
-			}
+			transitions = askForInt();
 		}
-		initialMarking = new int[places];
-		processInput();
+		initialMarking = new int[places]; 
+		getTransFromUser(); // Get IO for transitions and display result
+		getMarkingFromUser(); // Get Initial Marking from user and display result
 		outputString = "\nPlaces: " + places + "\nTransitions: " + transitions + "\nInput: " + input + "\nOutput: " + output + "\nInitial Marking: " + Arrays.toString(initialMarking);
 		return outputString;
 	}
 	
-	public void processInput(){
+	public void getTransFromUser(){
 		Transition[] trans = new Transition[transitions];
 		String temp = "";
 		// Loop through IO for transitions based on # of places and transitions entered by the user
 		for(int i = 1; i <= transitions; i++){
-			System.out.println("Enter Input for t" + i);
 			trans[i-1] = new Transition(places);
+			//Input
+			System.out.println("Enter Input for t" + i);
 			temp = "t" + i + ": (";
 			for(int j = 1; j <= places; j++){
-				System.out.print("p" + j + ": ");
-				trans[i-1].getInput()[j-1] = scan.nextInt();
+				trans[i-1].getInput()[j-1] = -1;
+				while(trans[i-1].getInput()[j-1] < 0){
+					System.out.print("p" + j + ": ");
+					trans[i-1].getInput()[j-1] = askForInt();
+				}
 				if(j == places)	{temp += trans[i-1].getInput()[j-1] + ")";}
 				else			{temp += trans[i-1].getInput()[j-1] + ", ";}
 			}
 			System.out.println(temp);
+			input += temp;
+			
+			//Output
 			System.out.println("Enter Output for t" + i);
 			trans[i-1] = new Transition(places);
 			temp = "t" + i + ": (";
 			for(int j = 1; j <= places; j++){
-				System.out.print("p" + j + ": ");
-				trans[i-1].getOutput()[j-1] = scan.nextInt();
+				trans[i-1].getOutput()[j-1] = -1;
+				while(trans[i-1].getOutput()[j-1] < 0){
+					System.out.print("p" + j + ": ");
+					trans[i-1].getOutput()[j-1] = askForInt();
+				}
 				if(j == places)	{temp += trans[i-1].getOutput()[j-1] + ")";}
 				else			{temp += trans[i-1].getOutput()[j-1] + ", ";}
 			}
 			System.out.println(temp);
+			output += temp;
 		}
-		
+	}
+	
+	public void getMarkingFromUser(){
+		String temp = "";
 		// get user's initial marking
-		System.out.println("Enter initial marking");
-		temp = "Initial Marking: (";
-		for(int k = 1; k <= places; k++){
-			System.out.print("p" + k + ": ");
-			initialMarking[k-1] = scan.nextInt();
-			if(k == places)	{temp += initialMarking[k-1] + ")";}
-			else			{temp += initialMarking[k-1] + ", ";}
-		}
-		System.out.println(temp);
+				System.out.println("Enter initial marking");
+				temp = "Initial Marking: (";
+				for(int k = 1; k <= places; k++){
+					initialMarking[k-1] = -1;
+					while(initialMarking[k-1] < 0){
+						System.out.print("p" + k + ": ");
+						initialMarking[k-1] = askForInt();
+					}
+					if(k == places)	{temp += initialMarking[k-1] + ")";}
+					else			{temp += initialMarking[k-1] + ", ";}
+				}
+				System.out.println(temp);
 	}
 
+	// checks for valid input
+	public int askForInt(){
+		int answer = -1;
+		if(scan.hasNextInt()){
+			answer = scan.nextInt();
+		}
+		else {
+			scan.next();
+		}
+		return answer;
+	}
 	/**
 	 * @return the places
 	 */
